996a66891cba93ef1207e5edbe69a0ca349db4e6
Federico <82dife1bwi@hft-stuttgart.de>
2020-11-13 10:45:49 +0100

    [SP-57] Rebased to Master

--- Frontend/SPFrontend/src/app/app-routing.module.ts
+++ Frontend/SPFrontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NewModuleComponent} from "./components/new-module/new-module.component";
 import {HomePageComponent } from "./components/home-page/home-page.component";
 import  {TestComponent} from "./components/test/test.component";
 
+
+
+
 const routes: Routes = [
   {path: 'home-page', component: HomePageComponent},
-  {path: 'test-page', component: TestComponent}
+  {path: 'test-page', component: TestComponent},
+  {path: 'New-Module', component: NewModuleComponent} /*http://localhost:4200/New-Module*/
 ];
 
 @NgModule({
