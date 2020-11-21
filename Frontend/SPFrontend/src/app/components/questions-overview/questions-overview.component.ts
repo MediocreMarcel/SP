030dd365b0b57c0660211fbfc7b4dc8305a6189e
Marcel <marcel.heda@gmail.com>
2020-11-21 22:25:53 +0100

    [SP-66] add new design

--- Frontend/SPFrontend/src/app/components/questions-overview/questions-overview.component.ts
+++ Frontend/SPFrontend/src/app/components/questions-overview/questions-overview.component.ts
@@ -1,45 +1,62 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {QuestionsOverviewService} from "../../services/questions-overview-service.service";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
-    selector: 'app-questions-overview',
-    templateUrl: './questions-overview.component.html',
-    styleUrls: ['./questions-overview.component.css']
+  selector: 'app-questions-overview',
+  templateUrl: './questions-overview.component.html',
+  styleUrls: ['./questions-overview.component.css']
 })
 export class QuestionsOverviewComponent implements OnInit {
 
-    tiles: ModuleDTO[];
+  sortBy: string = "definition";
+  tiles: ModuleDTO[];
 
 
-    constructor(private service: QuestionsOverviewService) {
-        service.sendModuleToDB({
-            "username": "0",
-            "password": "pw",
-            "mail": "mail",
-            "name": "Hans-Peter",
-            "surname": "Mustermann"
-        }).subscribe(u => this.tiles = u);
+  constructor(private service: QuestionsOverviewService, public dialog: MatDialog) {
+    service.sendModuleToDB({
+      "username": "0",
+      "password": "pw",
+      "mail": "mail",
+      "name": "Hans-Peter",
+      "surname": "Mustermann"
+    }).subscribe(u => {
+      this.tiles = u;
+      this.sortChanged();
+    });
+  }
+
+  ngOnInit(): void {
+    this.sortChanged()
+  }
+
+  public sortChanged() {
+    if (this.sortBy === "definition") {
+      this.tiles.sort((a, b) => a.definition.localeCompare(b.definition));
+    } else if (this.sortBy === "module") {
+      this.tiles.sort((a, b) => a.course.localeCompare(b.course));
     }
-
-    ngOnInit(): void {
-    }
-
-    public sortChanged(value) {
-        if (value === "definition") {
-            this.tiles.sort((a, b) => a.definition.localeCompare(b.definition));
-        } else if (value === "course") {
-            this.tiles.sort((a, b) => a.course.localeCompare(b.course));
-        }
-    }
+  }
 
 
-    navigateToQuestionsCollection(questionCollectionName: string) {
+  navigateToQuestionsCollection(questionCollectionName: string) {
 
-    }
+  }
+
+  openCreateDialog(){
+    const dialogRef = this.dialog.open(CreateModuleDialog, {
+      width: '250px',
+
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 }
 
 export class ModuleDTO {
-    module_id: number;
-    course: string;
-    definition: string;
+  module_id: number;
+  course: string;
+  definition: string;
 }
