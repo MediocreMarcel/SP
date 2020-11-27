97e57ad95b1cad2dbed2ffc3ab0c0e02f8efe35e
Marcel Heda <ThinkPad@10.93.67.131>
2020-11-27 11:50:35 +0100

    [SP-67] created Frontend

--- Frontend/SPFrontend/src/app/app.module.ts
+++ Frontend/SPFrontend/src/app/app.module.ts
@@ -11,24 +11,24 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { LoginComponent } from './components/login/login.component';
-import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {
   CreateModuleDialog,
   QuestionsOverviewComponent
 } from './components/questions-overview/questions-overview.component';
 import {MatGridListModule} from "@angular/material/grid-list";
+import {MatCardModule} from "@angular/material/card";
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import {MatOptionModule} from "@angular/material/core";
 import {MatSelectModule} from "@angular/material/select";
-import {GroupByPipe} from "./components/questions-collection/group-by.pipe";
+import {QuestionsCollectionComponent} from "./components/questions-collection/questions-collection.component";
 import {MatExpansionModule} from "@angular/material/expansion";
 import {MatCheckboxModule} from "@angular/material/checkbox";
+import {GroupByPipe} from "./components/questions-collection/group-by.pipe";
+import {LoginComponent} from "./components/login/login.component";
 import {UserService} from "./shared/user.service";
-import {QuestionsCollectionComponent} from "./components/questions-collection/questions-collection.component";
 
 @NgModule({
   declarations: [
