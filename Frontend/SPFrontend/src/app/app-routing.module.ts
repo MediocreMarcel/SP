d3a007970856594a003e379037f26a6ce926b7d6
Marcel <marcel.heda@gmail.com>
2020-12-06 19:40:56 +0100

    fix routing

--- Frontend/SPFrontend/src/app/app-routing.module.ts
+++ Frontend/SPFrontend/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import {HomePageComponent} from './components/home-page/home-page.component';
 import {QuestionsOverviewComponent} from './components/questions-overview/questions-overview.component';
 import {LoginComponent} from './components/login/login.component';
 import {AuthGuard} from './auth.guard';
+import {CreateExamComponent} from "./components/create-exam/create-exam.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'home', component: HomePageComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent},
   {path: 'questions-overview', component: QuestionsOverviewComponent, canActivate: [AuthGuard]},
-  {path: 'questions-overview', component: QuestionsOverviewComponent}
+  {path: 'questions-overview', component: QuestionsOverviewComponent},
+  {path: 'create-exam', component: CreateExamComponent}
 ];
 
 @NgModule({
