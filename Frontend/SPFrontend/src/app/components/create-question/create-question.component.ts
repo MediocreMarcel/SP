4cb7b76c8076efa7942ca861c465476e52b3fe51
Marcel Heda <ThinkPad@10.93.67.131>
2020-12-30 19:45:47 +0100

    [SP-16] first integration of questionscollection

--- Frontend/SPFrontend/src/app/components/create-question/create-question.component.ts
+++ Frontend/SPFrontend/src/app/components/create-question/create-question.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CreateExamService} from "../../services/create-exam.service";
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from "@angular/cdk/drag-drop";
+import {ModuleDTO} from "../questions-overview/questions-overview.component";
+import {QuestionDto} from "../models/QuestionDto";
+import {GroupByPipe} from "../questions-collection/group-by.pipe";
 
 @Component({
   selector: 'app-create-question',
@@ -8,32 +11,49 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from "@angular/cdk/drag
   styleUrls: ['./create-question.component.css']
 })
 export class CreateQuestionComponent implements OnInit {
-  examTitle = "Unbenannte Klausur";
+  examTitle = "Unbenannte Klausur"
 
-  constructor() { }
+  module: ModuleDTO;
+
+  questionPool: QuestionDto[] = [];
+  questionPoolByCategory: any[] = [];
+
+  examContent: QuestionDto[] = [];
+
+  questionCategories: string[] = [];
+
+  JSON = JSON;
+
+  constructor(private examService: CreateExamService) {
+    this.module = new ModuleDTO(1, "WI", "Prog 2")
+    examService.getQuestionsFromDb(this.module).subscribe(retVal => {
+        this.questionPool = retVal;
+
+        let pipe = new GroupByPipe();
+        pipe.transform(retVal, "category").forEach(u => {
+          //crete array with dummy object at first position. This dummy object represents the name of the category
+          this.questionPoolByCategory.push([{"questionName": u.key, "isDisabled": true}].concat(u.value));
+          //push name of category in corresponding array
+          this.questionCategories.push(u.key);
+        });
+        console.log(this.questionPoolByCategory);
+      }
+    )
+  }
 
   ngOnInit(): void {
   }
 
 
-  questionPool = [
-    'Get to work',
-    'Pick up groceries',
-    'Go home',
-    'Fall asleep'
-  ];
-
-  examContent = [
-    'Get up',
-    'Brush teeth',
-    'Take a shower',
-    'Check e-mail',
-    'Walk dog'
-  ];
-
-  drop(event: CdkDragDrop<string[]>) {
+  //called if something has been dropped between questions and exam
+  drop(event: CdkDragDrop<QuestionDto[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else if(this.questionCategories.includes(event.container.id) && event.currentIndex == 0) {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.container.data.length);
     } else {
       transferArrayItem(event.previousContainer.data,
         event.container.data,
@@ -41,5 +61,6 @@ export class CreateQuestionComponent implements OnInit {
         event.currentIndex);
     }
   }
+
 }
 
